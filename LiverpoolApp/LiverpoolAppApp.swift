c036c090e9e86c520acced37cc314afa9836e9d8
Ricardo <ricardogarcia.lopez@hotmail.com>
2025-08-26 08:43:34 -0600

    Project to the github repository

---
+++ LiverpoolApp/LiverpoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiverpoolAppApp.swift
+//  LiverpoolApp
+//
+//  Created by Ricardo Garcia Lopez on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LiverpoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
